52dc8ff2deb5abb8c69f948929197779d055b990
Chokchai Phatharamalai <juacompe@set.(none)>
2013-01-19 00:28:07 +0700

    - can validate row and column

--- eight_queen.py
+++ eight_queen.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 
+QUEEN = u'♛'
+
 def get_board(queen_positions):
     output = u"""
       a b c d e f g h
@@ -17,7 +19,6 @@ def get_board(queen_positions):
     return output 
 
 def put_queen(board, position):
-    QUEEN = u'♛'
     column_indexes = {
         'a': 2,
         'b': 4,
@@ -38,3 +39,45 @@ def put_queen(board, position):
 
 def to_coordinate_pair(position):
     return position[0], position[1]
+
+def validate_board(board):
+    rows_valid = True
+    for row in range(1,9):
+        row_valid = validate_row(str(row), board) 
+        rows_valid = rows_valid and row_valid
+    columns_valid = True
+    columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    for column in columns:
+        column_valid = validate_column(column, board)
+        columns_valid = columns_valid and column_valid
+    return rows_valid and columns_valid
+
+def validate_row(row, board):
+    row_index = board.find(row)
+    row_starts = row_index + 1
+    row_ends = board.find('\n', row_starts)
+    row_to_validate = board[row_starts: row_ends]
+    return row_to_validate.count(QUEEN) <= 1 
+
+def validate_column(column, board):
+    column_to_validate = get_column(column, board) 
+    return column_to_validate.count(QUEEN) <= 1
+
+def get_column(column, board):
+    board = board.replace(' ', '')
+    indexes_of_the_column = get_column_indexes(column, board)
+    column_to_validate = ''.join([ board[index] for index in indexes_of_the_column ])
+    return column_to_validate 
+
+def get_column_indexes(column, board):
+    board = board.replace(' ', '')
+    column_indexes = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4,
+        'e': 5, 'f': 6, 'g': 7, 'h': 8,
+    }
+    column_index = column_indexes[column]
+    index_starts = board.find('8')
+    index_ends = len(board)
+    row_length = 10
+    tile_indexes = [ column_index + i for i in range(index_starts, index_ends, row_length) ]
+    return tile_indexes 
--- test_eight_queen.py
+++ test_eight_queen.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-from eight_queen import get_board, put_queen
+from eight_queen import get_board, put_queen, validate_board, get_column, get_column_indexes
 from unittest import TestCase
 
 empty_board = u"""
@@ -68,7 +68,124 @@ class TestPutQueen(TestCase):
         """
         assertLikes(self, expected, result)
 
+class TestValidateBoard(TestCase):
+    def test_empty_board_should_valid(self):
+        valid = validate_board(empty_board)
+        self.assertTrue(valid)
 
+    def test_board_with_2_queens_in_row_1_should_not_valid(self):
+        board = u"""
+          a b c d e f g h
+        8 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        7 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        6 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        5 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        4 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        3 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        2 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        1 ♛ ◻ ◼ ◻ ◼ ◻ ♛ ◻
+        """
+        valid = validate_board(board)
+        self.assertFalse(valid)
+
+    def test_board_with_2_queens_in_row_7_should_not_valid(self):
+        board = u"""
+          a b c d e f g h
+        8 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        7 ◼ ◻ ◼ ♛ ♛ ◻ ◼ ◻
+        6 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        5 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        4 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        3 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        2 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        1 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        """
+        valid = validate_board(board)
+        self.assertFalse(valid)
+
+    def test_board_with_2_queens_in_column_a_should_not_valid(self):
+        board = u"""
+          a b c d e f g h
+        8 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        7 ♛ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        6 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        5 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        4 ♛ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        3 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        2 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        1 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        """
+        valid = validate_board(board)
+        self.assertFalse(valid)
+
+
+class TestGetColumn(TestCase):
+    def test_get_column_a(self):
+        board = u"""
+          a b c d e f g h
+        8 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        7 ♛ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        6 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        5 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        4 ♛ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        3 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        2 ◻ ◼ ◻ ◼ ◻ ◼ ◻ ◼
+        1 ◼ ◻ ◼ ◻ ◼ ◻ ◼ ◻
+        """
+        column = get_column('a', board)
+        expected = u'◻♛◻◼♛◼◻◼'
+        self.assertEqual(expected, column)
+         
+
+class TestGetColumnIndexes(TestCase):
+    def test_get_column_indexes_of_a(self):
+        board = u"""
+         abcdefgh
+        8◻◼◻◼◻◼◻◼
+        7♛◻◼◻◼◻◼◻
+        6◻◼◻◼◻◼◻◼
+        5◼◻◼◻◼◻◼◻
+        4♛◼◻◼◻◼◻◼
+        3◼◻◼◻◼◻◼◻
+        2◻◼◻◼◻◼◻◼
+        1◼◻◼◻◼◻◼◻
+        """
+        result = get_column_indexes('a', board)
+        expected_indexes = [11, 21, 31, 41, 51, 61, 71, 81]
+        self.assertEquals(expected_indexes, result)
+ 
+    def test_get_column_indexes_of_b(self):
+        board = u"""
+         abcdefgh
+        8◻◼◻◼◻◼◻◼
+        7♛◻◼◻◼◻◼◻
+        6◻◼◻◼◻◼◻◼
+        5◼◻◼◻◼◻◼◻
+        4♛◼◻◼◻◼◻◼
+        3◼◻◼◻◼◻◼◻
+        2◻◼◻◼◻◼◻◼
+        1◼◻◼◻◼◻◼◻
+        """
+        result = get_column_indexes('b', board)
+        expected_indexes = [12, 22, 32, 42, 52, 62, 72, 82]
+        self.assertEquals(expected_indexes, result)
+
+    def test_get_column_indexes_of_h(self):
+        board = u"""
+         abcdefgh
+        8◻◼◻◼◻◼◻◼
+        7♛◻◼◻◼◻◼◻
+        6◻◼◻◼◻◼◻◼
+        5◼◻◼◻◼◻◼◻
+        4♛◼◻◼◻◼◻◼
+        3◼◻◼◻◼◻◼◻
+        2◻◼◻◼◻◼◻◼
+        1◼◻◼◻◼◻◼◻
+        """
+        result = get_column_indexes('h', board)
+        expected_indexes = [18, 28, 38, 48, 58, 68, 78, 88]
+        self.assertEquals(expected_indexes, result)
+  
 def assertLikes(test, expected, result):
     """
     Assert that 2 strings are equal if the whitespace is ignored
